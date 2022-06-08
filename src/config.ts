01d116587707028768f60cb511db4afd3bec30f2
reealy_ <van.pham2809@gmail.com>
2022-06-09 00:08:58 +0200

    commentaires et readme

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-// Bot config
+// Bot config :
+// 
+// NODE_ENV defines the environment the bot is running in
+// TOKEN_BOT is the discord bot token from the discord developer portal
+// CLIENT_ID is the client id of the bot
+
 const { NODE_ENV, TOKEN_BOT, CLIENT_ID } = process.env;
 
+// TOKEN_BOT is mandatory
 if (!TOKEN_BOT) {
 	throw new Error("Token not found");
 }
