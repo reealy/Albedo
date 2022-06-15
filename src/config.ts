3947e3888b184059fe7ef6cd499368636fe782c0
reealy_ <van.pham2809@gmail.com>
2022-06-15 18:36:20 +0200

    changement de librairie discord.js -> discord.ts

--- src/config.ts
+++ src/config.ts
@@ -7,16 +7,24 @@ dotenv.config();
 // NODE_ENV defines the environment the bot is running in
 // TOKEN_BOT is the discord bot token from the discord developer portal
 // CLIENT_ID is the client id of the bot
+// GUILD_ID is the guild id of the dev server 
 
-const { NODE_ENV, TOKEN_BOT, CLIENT_ID } = process.env;
+const { NODE_ENV, TOKEN_BOT, CLIENT_ID, GUILD_ID } = process.env;
 
 // TOKEN_BOT is mandatory
 if (!TOKEN_BOT) {
 	throw new Error("Token not found");
 }
 
+// GUILD_ID is mandatory if NODE_ENV === "development"
+// Used for development, it avoids deploying commands to all guilds
+if (NODE_ENV === "development" && !GUILD_ID) {
+	throw new Error("Guild ID not found");
+}
+
 export const config = {
 	env: NODE_ENV,
 	token: TOKEN_BOT,
-	clientId: CLIENT_ID
+	clientId: CLIENT_ID,
+	guildId: GUILD_ID
 };
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,76 @@
-import { Client, Intents } from "discord.js";
-import { config } from "./config";
+import "reflect-metadata";
 
-console.log("Starting bot...");
-// Create a new Discord client
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS,
-    ] 
+import { dirname, importx } from "@discordx/importer";
+import type { Interaction, Message } from "discord.js";
+import { Intents } from "discord.js";
+import { Client } from "discordx";
+import { config } from "./config.js";
+
+export const bot = new Client({
+	// To only use global commands (use @Guild for specific guild command), comment this line
+	botGuilds: [
+		(): string => {
+			let guild: string = "";
+			
+			if (config.env === "development") {
+				guild = config.guildId as string;
+			}
+
+			return guild;
+		}
+	],
+
+	// Discord intents
+	intents: [
+	Intents.FLAGS.GUILDS,
+	Intents.FLAGS.GUILD_MEMBERS,
+	Intents.FLAGS.GUILD_MESSAGES,
+	Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+	Intents.FLAGS.GUILD_VOICE_STATES,
+	],
+
+	// Debug logs are disabled in silent mode
+	silent: false,
+
+	// Configuration for @SimpleCommand
+	simpleCommand: {
+	prefix: "!",
+	},
 });
 
-client.once("ready", () => {
-    console.log("Bot is ready!");
+bot.once("ready", async () => {
+	// Make sure all guilds are cached
+	await bot.guilds.fetch();
+
+	// Synchronize applications commands with Discord
+	await bot.initApplicationCommands();
+
+	// Synchronize applications command permissions with Discord
+	await bot.initApplicationPermissions();
+
+	// To clear all guild commands, uncomment this line,
+	// This is useful when moving from guild commands to global commands
+	// It must only be executed once	
+	// await bot.clearApplicationCommands(
+	//    ...bot.guilds.cache.map((g) => g.id)
+	// );
+
+	console.log("Bot started");
 });
 
-// Login to Discord with your app's token
-client.login(config.token);
+bot.on("interactionCreate", (interaction: Interaction) => {
+	bot.executeInteraction(interaction);
+});
+
+bot.on("messageCreate", (message: Message) => {
+	bot.executeCommand(message);
+});
+
+async function run() {
+	// The following syntax should be used in the ECMAScript environment
+	await importx(dirname(import.meta.url) + "/{events,commands}/**/*.{ts,js}");
+	// Log in with your bot token
+	await bot.login(config.token);
+}
+
+run();
